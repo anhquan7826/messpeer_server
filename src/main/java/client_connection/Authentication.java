2a8586cbf8d1cf3f622601580ce4c9502cbefddf
xor <benghia1st@gmail.com>
2022-04-18 18:35:25 +0700

    rebase

---
+++ src/main/java/client_connection/Authentication.java
@@ -0,0 +1,10 @@
+package client_connection;
+
+public class Authentication {
+    public static boolean authenticate(String initMessage) {
+        // TODO: authenticate a client
+        String username = initMessage.split(":")[1].split(" ")[0];
+        String passwordHash = initMessage.split(":")[1].split(" ")[1];
+        return true;
+    }
+}

79171303027c6f6bf96496cb7558eae48e655d64
anhquan7826 <anhquan7826@gmail.com>
2022-05-12 20:33:53 +0700

    adding client function

--- src/main/java/client_connection/Client.java
+++ src/main/java/client_connection/Client.java
@@ -1,13 +1,14 @@
 package client_connection;
 
 import database.DbObject;
-import utils.MessageJoiner;
+import utils.Json;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Client {
     private Socket socket;
@@ -32,11 +33,13 @@ public class Client {
                         String initMessage = getMessage();
                         if (initMessage != null) System.out.println(username + ": " + initMessage);
                         else continue;
-                        clientAuthenticated = DbObject.getInstance().authenticate(initMessage);
+                        initMessage = initMessage.replaceFirst("INITIAL_MESSAGE:", "");
+                        HashMap<String, String> credential = Json.toHashMap(initMessage);
+                        clientAuthenticated = DbObject.getInstance().authenticate(credential);
                         if (clientAuthenticated) {
                             System.out.println(username + ": Authenticate success!");
                             sendMessage("AUTHENTICATE_SUCCESS");
-                            setUsername(initMessage.split(":")[1].split(" ")[0]);
+                            setUsername(credential.get("username"));
                             startCommunicate();
                         } else {
                             System.out.println(username + ": Authenticate failed!");
@@ -114,27 +117,22 @@ public class Client {
                             String message = getMessage();
                             if (message != null) System.out.println(username + ": " + message);
                             else continue;
-                            switch (message) {
-                                case "SEND_MESSAGE":
-                                    String username = message.split(":")[1].split(" ")[0];
-                                    String groupChatID = message.split(":")[1].split(" ")[1];
-                                    String[] temp = message.split(":")[1].split(" ");
-                                    String messageJson = MessageJoiner.join(temp, 2, temp.length);
 
-
-                                    break;
-                                case "GET_GROUPCHAT_LIST":
-                                    break;
-                                case "GROUP_CHAT_CREATE":
-                                    break;
-                                case "GROUP_CHAT_ADD":
-                                    break;
-                                case "GROUP_CHAT_KICK":
-                                    break;
-                                case "GROUP_CHAT_CHANGE_HOST":
-                                    break;
-                                case "GROUP_CHAT_DELETE":
-                                    break;
+                            String messageHeader = message.split(":")[0];
+                            if ("SEND_MESSAGE".equals(messageHeader)) {
+                                // TODO: send message
+                            } else if ("GET_GROUPCHAT_LIST".equals(messageHeader)) {
+                                HashMap<String, String> groupList = DbObject.getInstance().getGroupList(username);
+                                if (groupList == null) {
+                                    sendMessage("GET_GROUPCHAT_LIST_ERROR");
+                                } else {
+                                    sendMessage("GET_GROUPCHAT_LIST_OK:" + Json.toJson(groupList));
+                                }
+                            } else if ("GROUP_CHAT_CREATE".equals(messageHeader)) {
+                            } else if ("GROUP_CHAT_ADD".equals(messageHeader)) {
+                            } else if ("GROUP_CHAT_KICK".equals(messageHeader)) {
+                            } else if ("GROUP_CHAT_CHANGE_HOST".equals(messageHeader)) {
+                            } else if ("GROUP_CHAT_DELETE".equals(messageHeader)) {
                             }
                         } catch (Exception e) {
                             throw new RuntimeException(e);
@@ -144,5 +142,4 @@ public class Client {
             }
         }).start();
     }
-
 }
